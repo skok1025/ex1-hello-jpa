c6f0f6ffe7c173a29657e1e1ccd594c69a88d077
shkim <skok1025@naver.com>
2021-09-24 16:04:15 +0900

    JPA START

---
+++ src/main/java/hello/jpa/JpaMain.java
@@ -0,0 +1,51 @@
+package hello.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+	public static void main(String[] args) {
+		EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+		
+		EntityManager em = enf.createEntityManager();
+		
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		
+		try {
+//			INSERT
+//			Member member = new Member();
+//			member.setId(2L);
+//			member.setName("helloB");
+//			em.persist(member);
+			
+//			UPDATE
+			Member findMember = em.find(Member.class, 1L);
+			findMember.setName("Hello JPA");
+			
+//			SELECT
+			List<Member> resultList = 
+					em.createQuery("SELECT m FROM Member as m", Member.class)
+					.setFirstResult(0)
+					.setMaxResults(2)
+					.getResultList();
+			
+			for (Member member : resultList) {
+				System.out.println(member);
+			}
+			
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			em.close();
+		}
+		
+		enf.close();
+	}
+
+}
